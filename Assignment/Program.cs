b0eaa64962564cbfc52d377ae3c05c31e863dce0
Ivan-NKY <151420870+Ivan-NKY@users.noreply.github.com>
2024-01-24 15:29:31 +0000

    added new stuff in program.cs

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -3,3 +3,23 @@
 // Student Name : Gan Jun Hang
 // Partner Name : Ivan Ng Keyang
 //==========================================================
+
+using Assignment;
+
+List <Customer> CustomerList = new List<Customer>();
+using (StreamReader sr = new StreamReader("customers.csv")) 
+{
+    sr.ReadLine();  //removing header
+    string line;
+
+    Console.WriteLine($"Name\tMember ID\tDate of Birth\tMembershipStatus\tMembershipPoints\tPunchCard");
+    while ((line = sr.ReadLine()) != null)
+    {
+        string[] values = line.Split(',');
+        Console.WriteLine($"{values[0]}\t{values[1]}\t{values[2]}\t{values[3]}\t{values[4]}\t{values[5]}");
+    }
+
+    
+    sr.Close();
+}
+        
