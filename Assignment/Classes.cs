a5766db75902b5cf7394b6325b5335fdea36fa90
DasMeAyy <64674439+gjh0239@users.noreply.github.com>
2024-01-24 05:20:07 +0000

    Initil commit for Flavour & Topping, Changed IceCream & Cup

--- Assignment/Classes.cs
+++ Assignment/Classes.cs
@@ -2,12 +2,12 @@ namespace Assignment
 {
 	internal class IceCream
 	{
-		internal string Option { get; set; }
-		internal int Scoops { get; set; }
-		internal List<Flavour> Flavours { get; set; }
-		internal List<Topping> Toppings { get; set; }
+		public string Option { get; set; }
+		public int Scoops { get; set; }
+		public List<Flavour> Flavours { get; set; }
+		public List<Topping> Toppings { get; set; }
 
-		internal IceCream()
+		public IceCream()
 		{
 			Option = "";
 			Scoops = 0;
@@ -15,7 +15,7 @@ namespace Assignment
 			Toppings = new List<Topping>();
 		}
 
-		internal IceCream(string option, int scoops, List<Flavour> flavours, List<Topping> toppings)
+		public IceCream(string option, int scoops, List<Flavour> flavours, List<Topping> toppings)
 		{
 			Option = option;
 			Scoops = scoops;
@@ -25,26 +25,84 @@ namespace Assignment
 
 	}
 
-	internal class Cup: IceCream
+	internal class Flavour
 	{
-		internal Cup(): base() {}
+		public string Type { get; set; }
+		public bool Premium { get; set; }
+		public int Quantitiy { get; set; }
 
-		internal Cup(string option, int scoops, List<Flavour> flavours, List<Topping> toppings):
-				base(option, scoops, flavours, toppings) {}
-		
-		internal double CalculatePrice()
+		public Flavour()
 		{
-			double price = 0;
-
-			price += toppings.Count * 0.5;
-
-			foreach (Flavour flavour in Flavours)
-			{
-				// TODO: Implement price calculation 
-			}
-
+			Type = "";
+			Premium = false;
+			Quantitiy = 0;
 		}
 
-		
+		public Flavour(string type, bool premium, int quantitiy)
+		{
+			Type = type;
+			Premium = premium;
+			Quantitiy = quantitiy;
+		}
+
+		public override string ToString()
+		{
+			return $"{Type}: {Quantitiy}";
+		}
+	}
+
+	internal class Topping
+	{
+		public string Type { get; set; }
+
+		public Topping()
+		{
+			Type = "";
+		}
+
+		public Topping(string type, int quantitiy)
+		{
+			Type = type;
+		}
+
+		public override string ToString()
+		{
+			return Type;
+		}
+	}
+
+	internal class Cup : IceCream
+	{
+		public Cup() : base() { }
+
+		public Cup(string option, int scoops, List<Flavour> flavours, List<Topping> toppings) :
+				base(option, scoops, flavours, toppings) {}
+
+		public double CalculatePrice()
+		{
+			double price;
+
+			switch (Scoops) // Scoop price implementation
+			{
+				default: price = 4.0; break;
+				case 2: price = 5.5; break;
+				case 3: price = 6.5; break;
+			}
+
+			// Add-ons price implementation
+			price += Toppings.Count * 1; // $1/topping 
+
+			foreach (Flavour flavour in Flavours) // Flavour price implementation
+			{
+				if (flavour.Premium) // Premium flavour +$2/scoop
+				{
+					price += 2*Scoops;
+				}
+			}
+
+			return price;
+		}
+
+
 	}
 }
